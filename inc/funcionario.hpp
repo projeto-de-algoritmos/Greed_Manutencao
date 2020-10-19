3e1223f19b5a46ec5fa0a25d80314a66e487d1d7
Kleidson <kleidsonalves15@gmail.com>
2020-10-19 19:26:32 -0300

    Criação da classe Funcionário

---
+++ inc/funcionario.hpp
@@ -0,0 +1,25 @@
+#ifndef FUNCIONARIO_HPP
+#define FUNCIONARIO_HPP
+
+#include <iostream>
+#include <vector>
+
+#include "tarefa.hpp"
+
+using namespace std; 
+
+class Funcionario{
+private: 
+    vector <Tarefa> listaTarefas; 
+    int horaDisponivel;
+    int minDisponivel; 
+
+public: 
+    Funcionario(Tarefa tarefa);
+    void setHora(int hora); 
+    void setMin(int min);  
+
+};
+
+
+#endif
